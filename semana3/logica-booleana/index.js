0b5ace0d45a50ad883c9d3adaef84144040c0872
Luiz <luizmitsuru@gmail.com>
2020-04-27 18:01:50 -0300

    Execicios terminados

--- semana3/logica-booleana/index.js
+++ semana3/logica-booleana/index.js
@@ -1,4 +1,4 @@
-const bool1 = true
+/*const bool1 = true
 const bool2 = false
 const bool3 = !bool2
 
@@ -40,7 +40,7 @@ console.log('VI. 3 ', array[i])
 i = array.length - 1
 array[i] = array[i-3]
 const resultadoC = array[i]%array[1]
-console.log('VII. 1 ', resultadoC)
+console.log('VII. 1 ', resultadoC)*/
 
 /*a. O que é array e como se declara em JS?
 
@@ -60,3 +60,86 @@ d. Indique todas as mensagens impressas no console.
 Usa o console.log para imprimir todas as mensagens no console.
 
 */
+
+
+//Graus Fahrenheit(°F) para Kelvin(K)
+
+
+//a. Calcule e mostre o valor de 77°F em  K, 
+//mostrando a unidade no console também.
+//Graus Celsius(°C) para Graus Fahrenheit (°C)
+//let GRAUS_FAHRENHEIT = (GRAUS_CELSIUS)*9/5 + 32
+
+/*let KELVIN = (77- 32)*5/9 + 273.15
+console.log(KELVIN)*/
+//a 298.15k
+//b. Calcule e mostre o valor de 80°C em °F, mostrando a unidade no console também.
+
+/*let GRAUS_FAHRENHEIT = (80)*9/5 + 32
+console.log(GRAUS_FAHRENHEIT)
+//b- 176f*/
+
+//c. Calcule e mostre o valor de 30°C em °F e K, mostrando as unidades no console também.
+
+/*let KELVIN = (30- 32)*5/9 + 273.15
+let GRAUS_FAHRENHEIT = (30)*9/5 + 32
+console.log(KELVIN)
+console.log(GRAUS_FAHRENHEIT)
+//272k e 86 F*/
+
+//d. Altere o último item para que o usuário insira o valor em graus Celsius que ele deseja converter.
+
+/*let GRAUS_CELSIUS = prompt("Digite quantos graus C deseja")
+
+let GRAUS_FAHRENHEIT = (GRAUS_CELSIUS)*9/5 + 32
+console.log(GRAUS_CELSIUS)
+console.log(GRAUS_FAHRENHEIT)*/
+
+/*let endereco = prompt("Qual é o seu endereço?")
+console.log("Seu endereço é ",endereco)
+let cor = prompt("Qual a sua cor favorita?")
+console.log("Sua cor favorita é ",cor)
+let animal = prompt("Qual é o seu animal favorito?")
+console.log("Seu animal favorito é ", animal)
+const idade = prompt("Qual é a sua idade?")
+console.log("Sua idade é ", idade +" anos")
+const nome = prompt("Qual é o seu nome?")
+console.log("Bem Vindo ", nome)*/
+
+/*Quilowatt-hora é uma unidade de energia; e é muito utilizada para se determinar o consumo 
+de energia elétrica em residências. Sabe-se que o quilowatt-hora de energia custa R$0.05.
+ Faça um programa que receba a quantidade de quilowatts consumida por uma residência.*/
+
+ 
+
+ /*const quilowattHoraValor = 0.05
+  let quilowattConsumido = prompt("Quantos quilowatts você consumiu?")
+ console.log(quilowattConsumido)
+ let valorAserPago = Number(quilowattConsumido) *0.05
+ console.log(valorAserPago)
+ let desconto = valorAserPago * 0.15
+ let valorComDesconto = valorAserPago - desconto
+ console.log(valorComDesconto)*/
+
+ //a. Calcule e mostre o valor a ser pago por uma residência que consuma 280 quilowatt-hora;
+ /*const quilowattHoraValor = 0.05
+  let quilowattConsumido = 280
+ console.log(quilowattConsumido)
+ let valorAserPago = quilowattConsumido *0.05
+ console.log(valorAserPago)*/
+ 
+ /*b. Altere o programa para receber mais um valor: a porcentagem de desconto. 
+ Calcule e mostre o valor a ser pago pela mesma residência acima
+  considerando 15% de desconto.*/
+  
+  /*const quilowattHoraValor = 0.05
+  let quilowattConsumido = 280
+  console.log(quilowattConsumido)
+  let valorAserPago = quilowattConsumido *0.05
+  console.log(valorAserPago)
+  let desconto = valorAserPago * 0.15
+  let valorComDesconto = valorAserPago - desconto
+  console.log(valorComDesconto)*/
+ 
+
+

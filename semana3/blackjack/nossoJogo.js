0aa9c9e463d5a0e0efa089c9713d2d9a65b5b9b2
Luiz <luizmitsuru@gmail.com>
2020-05-03 12:23:13 -0300

    Exercício finalizado

--- semana3/blackjack/nossoJogo.js
+++ semana3/blackjack/nossoJogo.js
@@ -1,12 +1,29 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+let boasVindas = confirm("Bem Vindo ao jogo deseja iniciar uma partida?")
+let jogar = boasVindas
+const carta = comprarCarta();
+const carta2 = comprarCarta(); 
+// Sorteia uma carta. Por exemplo, o rei de ouros
+let usuario = comprarCarta()
+let computador = comprarCarta()
+let valor = carta.valor + carta.valor
+  
+ 
+  if((jogar=== true)) {
+     console.log(` Usuário carta  ${carta.valor} ${carta.texto}  valor ${valor}`) // o que fazer se o usuário clicar "ok"
+     console.log(` Computador carta  ${carta2.valor} ${carta2.texto} valor ${valor}`)
+   } else if (jogar=== false) {
+      console.log("O jogo acabou")
+   } if (carta>carta2){
+      console.log(`Venceu Usuário carta  ${carta.valor} ${carta.texto}  valor ${valor}`)
+   }else if(carta2>carta){
+      console.log(`Venceu Computador carta  ${carta2.valor} ${carta2.texto} valor ${valor}`)
+   }else{
+      console.log(`Empate Usuário carta  ${carta.valor} ${carta.texto}  valor ${valor} Computador carta  ${carta2.valor} ${carta2.texto} valor ${valor}` )
+   }
+   // o que fazer se o usuário clicar "cancelar"
+   
+ 
     
     console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
     console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+ 

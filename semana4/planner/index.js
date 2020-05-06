31a2666b7180f95d110a85cf3bd1dbe9b15fefa1
Luiz <luizmitsuru@gmail.com>
2020-05-06 01:25:37 -0300

    Exercicio atualizado

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,7 +1,17 @@
 function criarTarefa() {
-   const input = document.querySelector("input");
+   const meuInput = document.querySelector("meuInput").value;
 }
-const novaTarefa = input.value;
+const novaTarefa = meuInput.value;
 if (novaTarefa==="") {
     alert("Coloque uma tarefa");
 }
+
+const listaSemana = document.querySelector(".semanaTamanho");
+const lista = listaSemana.value;
+console.log(lista);
+
+let listaTarefas = document.querySelector("#" + lista + "ul-tarefas");
+console.log(listaTarefas);
+listaTarefas.innerHTML += "<li>" + novaTarefa +"</li>" ;
+console.log(listaTarefas);
+

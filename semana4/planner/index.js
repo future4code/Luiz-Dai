8bfb253e2e1c1f9d9a88fce8cd507e8e4f79dd7d
Luiz <luizmitsuru@gmail.com>
2020-05-06 02:43:03 -0300

    Exercicio finalizado

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,17 +1,30 @@
 function criarTarefa() {
-   const meuInput = document.querySelector("meuInput").value;
+    let tarefa = document.getElementById("novaTask").value;
+    let semana = document.getElementById("diaDaSemana").value;
+   
+    
+switch (semana) {
+    
+    case "segunda":
+        document.getElementById("segunda").innerHTML +=`<li>${tarefa}</li>`
+        break;
+    case "terca":
+        document.getElementById("terca").innerHTML +=`<li>${tarefa}</li>`
+        break;
+    case "quarta":
+        document.getElementById("quarta").innerHTML +=`<li>${tarefa}</li>`
+        break;
+    case "quinta":
+        document.getElementById("quinta").innerHTML +=`<li>${tarefa}</li>`
+        break;
+    case "sexta":
+        document.getElementById("sexta").innerHTML +=`<li>${tarefa}</li>`
+        break;
+    case "sabado":
+        document.getElementById("sabado").innerHTML +=`<li>${tarefa}</li>`
+        break;
+    default:
+        document.getElementById("domingo").innerHTML +=`<li>${tarefa}</li>`
+        break;
 }
-const novaTarefa = meuInput.value;
-if (novaTarefa==="") {
-    alert("Coloque uma tarefa");
 }
-
-const listaSemana = document.querySelector(".semanaTamanho");
-const lista = listaSemana.value;
-console.log(lista);
-
-let listaTarefas = document.querySelector("#" + lista + "ul-tarefas");
-console.log(listaTarefas);
-listaTarefas.innerHTML += "<li>" + novaTarefa +"</li>" ;
-console.log(listaTarefas);
-

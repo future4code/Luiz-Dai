e197edd12f754c5ebe94ee2c92643a57e892dfe2
Luiz <luizmitsuru@gmail.com>
2020-05-07 23:08:36 -0300

    html atualizado

---
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -0,0 +1,16 @@
+/*A primeira seção é uma de cadastro, 
+que deve solicitar ao usuário, ao menos,
+ essas 3 informações: valor (em reais); 
+ tipo da despesa (em um dropdown para ele selecionar) 
+ e um campo de descrição. Deve haver, 
+ao menos, 3 tipos de despesa; vocês podem escolher 
+quais serão esses tipos, mas aqui há exemplos de
+ alguns:
+  "Despesas de Casa", "Despesas de Festas", 
+  "Despesas de Viagem", "Despesas com Alimentação"
+  , "Despesas com transporte", "Outros", etc.
+   Limitem-se a utilizar valores inteiros
+    para os gastos (ou seja, usar "60" e não usar "60.5") */
+
+    let valor = input("Digite um valor")
+    let tipoDespesa = input("Digite o tipo de despesa")

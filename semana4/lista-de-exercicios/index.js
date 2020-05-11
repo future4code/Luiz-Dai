c153bb479fa63e676a20f38dc940b36f0ac7fafa
Luiz <luizmitsuru@gmail.com>
2020-05-11 01:00:56 -0300

    Exercicio 1 finalizado

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,9 @@
+//Exercício 1 fianalizado
+//Cite 3 maneiras de se percorrer/iterar uma lista. Faça um programa para exemplificar.
+
+//Podemos percorrer uma lista usando o for, foreach e o for of.
+
+/*const numeros = [15,17,99,98,74]
+for(let numero of numeros){
+    console.log(numero)
+}*/

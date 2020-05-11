5cc0b1c1c39b7b0d54d8cd7278f6944e7d44609f
Luiz <luizmitsuru@gmail.com>
2020-05-11 01:47:51 -0300

    Exercicoe 4

--- semana4/lista-de-exercicios/index.js
+++ semana4/lista-de-exercicios/index.js
@@ -14,3 +14,29 @@ for(let numero of numeros){
 //c false
 //d true
 //e false
+
+/*let quantidadeDeNumerosPares=[]
+let i = 1
+while(quantidadeDeNumerosPares) {
+  
+    i++
+    console.log(i*2)
+}
+//ele dá um loop infinito*/
+
+//exercicio 4
+
+let a = prompt("digite um numero")
+let b = prompt("digite outro numero")
+let c = (prompt("digite mais um numero"))
+ let n1 = Number(a)
+ let n2 =Number(b)
+ let n3= Number(c)
+if (n1===n2===n3) {
+     console.log("Equilatero")
+ }else if (n1!=n2===n3){
+     console.log("Isosxeles")
+ }
+     
+
+ 

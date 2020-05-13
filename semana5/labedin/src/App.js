9d2e179160c28bb1e763355847fb97d85dc04959
Luiz <luizmitsuru@gmail.com>
2020-05-12 23:22:30 -0300

    Exercicio realizado

--- semana5/labedin/src/App.js
+++ semana5/labedin/src/App.js
@@ -10,8 +10,8 @@ function App() {
         <h2>Dados pessoais</h2>
         <CardGrande 
           imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          nome="Luiz" 
+          descricao="Oi, eu sou o Luiz. Sou aluno da Labenu. Adoro o curso e estou me empenhando para aprender cada vez mais."
         />
         
         <ImagemButton 
@@ -25,7 +25,7 @@ function App() {
         <CardGrande 
           imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
           nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+          descricao="Cursando !" 
         />
         
         <CardGrande 

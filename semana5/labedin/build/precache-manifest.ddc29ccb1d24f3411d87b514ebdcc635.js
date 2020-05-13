5b41f7d1db82d2c9b7ec60827510c647ef03a32b
Luiz <luizmitsuru@gmail.com>
2020-05-13 17:02:14 -0300

    Exercicio realizado

---
+++ semana5/labedin/build/precache-manifest.ddc29ccb1d24f3411d87b514ebdcc635.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11a593e7e837fa6164a941f303117aae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ed3feb898f405e1be80",
+    "url": "/static/css/main.cb4ecaf2.chunk.css"
+  },
+  {
+    "revision": "dd35cab1ec671b13bbed",
+    "url": "/static/js/2.65cf7749.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.65cf7749.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ed3feb898f405e1be80",
+    "url": "/static/js/main.d4ee67ec.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  }
+]);

cddf183e073da9da13650eaa0c26dc6efd73b799
Luiz <luizmitsuru@gmail.com>
2020-05-18 23:08:52 -0300

    Exercicio atualicado

--- semana6/renderizacao-condicional/formulario-app/src/App.js
+++ semana6/renderizacao-condicional/formulario-app/src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Etapa1 from "./components/Etapa1";
+import Etapa2 from "./components/Etapa2";
+import Etapa3 from "./components/Etapa3";
+import Final from "./components/Final";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+          <div>
+            {this.renderizaEtapa()}
+            <button>Próxima Etapa</button>
+          </div>
+        );
+  }
+
+    state = {
+      etapa: 1,
+    }
+
+    class App extends React.Component {
+      state = {
+        etapa: 1,
+      }
+    
+      renderizaEtapa = () => {
+        switch (this.state.evento) {
+          case 1: 
+            return <Etapa1 />;
+          case 2: 
+            return <Etapa2 />;
+            case 3: 
+            return <Etapa3 />;
+          case 4: 
+            return <Final />;
+      // de acordo com o que já foi feito, 
+      // crie o case dos componentes etapa 3 e Final
+        }
+      }
 
 export default App;
---
+++ semana6/renderizacao-condicional/formulario-app/src/components/Etapa2.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Etapa2() {
+    return (
+      <div >
+        <h1>ETAPA-2 INFORMAÇÕES DO ENSINO SUPERIOR</h1>
+        <p>5-Qual curso?</p>
+        <input></input>
+        <p>6-Qual unidade de ensino?</p>
+        <input></input>
+       
+      </div>
+    );
+  }
+  
+  export default Etapa2;
+  

b22408380e95b8505b3f3d8a0ac89078820c0a91
Luiz <luizmitsuru@gmail.com>
2020-06-05 15:02:18 -0300

    Exercicio iniciado

---
+++ semana7/labefy/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import './Player/PlayList';
+import PlayList from './Player/PlayList';
+function App() {
+  return (
+    <div className="App">
+      <PlayList />
+    </div>
+  );
+}
+
+export default App;
---
+++ semana7/labefy/src/Player/PlayList.js
@@ -0,0 +1,61 @@
+import React from "react";
+import axios from "axios";
+
+class PlayList extends React.Component {
+    state = {
+        name: "",
+
+    };
+
+    handleNameChange = event => {
+        const newNameValue = event.target.value;
+
+        this.setState({ name: newNameValue });
+    };
+
+
+    handleCreateUser = () => {
+        const axiosConfig = {
+            headers: {
+                Authorization: "Luiz Mitsuru Dai - Mello"
+            }
+        };
+
+        const body = {
+            name: this.state.name,
+
+        };
+
+        axios
+            .post(
+                "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+                body,
+                axiosConfig
+            )
+            .then(() => {
+                alert(`Usuário ${this.state.name} criado com sucesso!`);
+                this.setState({ name: "" });
+            })
+            .catch(error => {
+                alert("Erro ao criar a playlist");
+                console.log(error);
+            });
+    };
+
+    render() {
+        return (
+            <div>
+                <input
+                    placeholder="PlayList"
+                    type="text"
+                    value={this.state.name}
+                    onChange={this.handleNameChange}
+                />
+
+                <button onClick={this.handleCreateUser}>Criar</button>
+            </div>
+        );
+    }
+}
+
+export default PlayList;

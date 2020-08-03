3a876bd443e4f99611c0d5aec56a5b4677a61f33
Luiz <luizmitsuru@gmail.com>
2020-08-03 04:18:46 -0300

    finalizado

--- semana14/projeto-bank/template-sistema-bancario/src/index.ts
+++ semana14/projeto-bank/template-sistema-bancario/src/index.ts
@@ -1,20 +1,63 @@
-import * as fs from 'fs'
+import * as fs from "fs";
+import moment from "moment";
+
+type conta = {
+  nome: string;
+  cpf: string;
+  dataNascimento: string;
+  saldo: number;
+};
+
+type extrato = [
+  {
+    valor: number;
+    data: string;
+    descricao: string;
+  }
+];
+
+const nomeDigitado: string = process.argv[2];
+const cpfDigitado: string = process.argv[3];
+const dataDigitada: string = process.argv[4];
 
 export function readDatabase(): any {
   try {
-    const fileData: string = fs.readFileSync('./data.json').toString()
-    return JSON.parse(fileData)
+    const fileData: string = fs.readFileSync("./data.json").toString();
+    return JSON.parse(fileData);
   } catch (error) {
-    console.log("Erro ao ler a base de dados: " + error.message)
-    return []
+    console.log("Erro ao ler a base de dados: " + error.message);
+    return [];
   }
 }
 
+const dados = readDatabase();
+
 export function writeToDatabase(data: any): void {
   try {
-    const dataAsString: string = JSON.stringify(data, null, 2)
-    fs.writeFileSync('./data.json', dataAsString)
+    for (let dado of dados) {
+      if (dado.cpf === cpfDigitado) {
+        return console.log("CPF já existe");
+      }
+    }
+    if (moment().diff(moment(dataDigitada, "DD/MM/YYYY"), "years") < 18) {
+      console.log("Idade mínima para criação de conta: 18 anos");
+    } else {
+      const addDados = () => {
+        dados.push({
+          nome: nomeDigitado,
+          cpf: cpfDigitado,
+          dataNascimento: dataDigitada,
+          saldo: 0,
+          extratoUsuario: [{ valor: 0, data: "18/07/2020", descricao: "game" }],
+        });
+      };
+      addDados();
+      const dataAsString: string = JSON.stringify(data, null, 2);
+      fs.writeFileSync("./data.json", dataAsString);
+    }
   } catch (error) {
-    console.log("Erro ao salvar os dados: " + error.message)
+    console.log("Erro ao salvar os dados: " + error.message);
   }
 }
+
+writeToDatabase(dados);
